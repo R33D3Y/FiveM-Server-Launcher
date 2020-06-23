900f148bf9537e22a74ad63f2e2a030b411f5bd0
R33D3Y <reedey2708@gmail.com>
2020-06-23 18:00:21 +0100

    ClickOnce Publish Link

--- FiveMServerLauncher/Properties/AssemblyInfo.cs
+++ FiveMServerLauncher/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyTitle("FiveMServerLauncher")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("R33D3Y")]
 [assembly: AssemblyProduct("FiveMServerLauncher")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
@@ -51,5 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.4.*")]
+#pragma warning disable CS7035 // The specified version string does not conform to the recommended format - major.minor.build.revision
+[assembly: AssemblyFileVersion("0.0.4.*")]
+#pragma warning restore CS7035 // The specified version string does not conform to the recommended format - major.minor.build.revision
